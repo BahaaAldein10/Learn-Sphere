01e717b41ce1684eef9fb4cc9d50f0e3f7ba4775
Bahaa Aldein <seniorwizard10@gmail.com>
2025-04-12 09:41:49 +0200

    last commit

--- src/app/(home)/(routes)/(student)/collaboration/page.tsx
+++ src/app/(home)/(routes)/(student)/collaboration/page.tsx
@@ -1,13 +1,13 @@
-import AddDocumentBtn from '@/components/shared/AddDocumentBtn';
-import { DeleteModal } from '@/components/shared/DeleteModal';
-import { getDocuments } from '@/lib/actions/room.actions';
-import { dateConverter } from '@/lib/utils';
-import { currentUser } from '@clerk/nextjs/server';
-import { RoomData } from '@liveblocks/node';
+// import AddDocumentBtn from '@/components/shared/AddDocumentBtn';
+// import { DeleteModal } from '@/components/shared/DeleteModal';
+// import { getDocuments } from '@/lib/actions/room.actions';
+// import { dateConverter } from '@/lib/utils';
+// import { currentUser } from '@clerk/nextjs/server';
+// import { RoomData } from '@liveblocks/node';
 import { Metadata } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Collaboration | LearnSphere',
@@ -18,71 +18,72 @@ export const metadata: Metadata = {
 };
 
 const Collaboration = async () => {
-  const clerkUser = await currentUser();
-  if (!clerkUser) return redirect('/');
+  // const clerkUser = await currentUser();
+  // if (!clerkUser) return redirect('/');
 
-  const roomDocuments = await getDocuments(
-    clerkUser.emailAddresses[0].emailAddress
-  );
+  // const roomDocuments = await getDocuments(
+  //   clerkUser.emailAddresses[0].emailAddress
+  // );
 
   return (
-    <main className="flex-center p-6">
-      {roomDocuments && roomDocuments.data.length > 0 ? (
-        <div className="document-list-container">
-          <div className="document-list-title">
-            <h3 className="text-2xl font-semibold">All documents</h3>
-            <AddDocumentBtn
-              userId={clerkUser.id}
-              email={clerkUser.emailAddresses[0].emailAddress}
-            />
-          </div>
+    <>Coming Soon</>
+    // <main className="flex-center p-6">
+    //   {roomDocuments && roomDocuments.data.length > 0 ? (
+    //     <div className="document-list-container">
+    //       <div className="document-list-title">
+    //         <h3 className="text-2xl font-semibold">All documents</h3>
+    //         <AddDocumentBtn
+    //           userId={clerkUser.id}
+    //           email={clerkUser.emailAddresses[0].emailAddress}
+    //         />
+    //       </div>
 
-          <ul className="document-ul">
-            {roomDocuments.data.map(({ id, metadata, createdAt }: RoomData) => (
-              <li key={id} className="document-list-item">
-                <Link
-                  href={`/collaboration/documents/${id}`}
-                  className="flex flex-1 items-center gap-4"
-                >
-                  <div className="hidden rounded-md bg-purple-200 p-2 sm:block">
-                    <Image
-                      src="/assets/icons/doc.png"
-                      alt="file"
-                      width={40}
-                      height={40}
-                    />
-                  </div>
-                  <div className="space-y-1">
-                    <p className="line-clamp-1 text-lg font-semibold">
-                      {metadata.title}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                      Created about {dateConverter(createdAt)}
-                    </p>
-                  </div>
-                </Link>
-                <DeleteModal roomId={id} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div className="document-list-empty">
-          <Image
-            src="/assets/icons/doc.png"
-            alt="Document"
-            width={50}
-            height={50}
-            className="mx-auto"
-          />
+    //       <ul className="document-ul">
+    //         {roomDocuments.data.map(({ id, metadata, createdAt }: RoomData) => (
+    //           <li key={id} className="document-list-item">
+    //             <Link
+    //               href={`/collaboration/documents/${id}`}
+    //               className="flex flex-1 items-center gap-4"
+    //             >
+    //               <div className="hidden rounded-md bg-purple-200 p-2 sm:block">
+    //                 <Image
+    //                   src="/assets/icons/doc.png"
+    //                   alt="file"
+    //                   width={40}
+    //                   height={40}
+    //                 />
+    //               </div>
+    //               <div className="space-y-1">
+    //                 <p className="line-clamp-1 text-lg font-semibold">
+    //                   {metadata.title}
+    //                 </p>
+    //                 <p className="text-sm text-gray-600">
+    //                   Created about {dateConverter(createdAt)}
+    //                 </p>
+    //               </div>
+    //             </Link>
+    //             <DeleteModal roomId={id} />
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   ) : (
+    //     <div className="document-list-empty">
+    //       <Image
+    //         src="/assets/icons/doc.png"
+    //         alt="Document"
+    //         width={50}
+    //         height={50}
+    //         className="mx-auto"
+    //       />
 
-          <AddDocumentBtn
-            userId={clerkUser.id}
-            email={clerkUser.emailAddresses[0].emailAddress}
-          />
-        </div>
-      )}
-    </main>
+    //       <AddDocumentBtn
+    //         userId={clerkUser.id}
+    //         email={clerkUser.emailAddresses[0].emailAddress}
+    //       />
+    //     </div>
+    //   )}
+    // </main>
   );
 };
 
